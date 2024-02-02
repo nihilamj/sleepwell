318c43abb8c9266010a57832cc13c4b52137dbf9
nihilamj <nihilamjmj@gmail.com>
2024-02-02 19:23:11 +0530

    Update views.py

--- sleepwell/mainapp/views.py
+++ sleepwell/mainapp/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, world. You're at the Main HOME page.")
+    return render(request, 'mainapp/home.html', {'page': 'home'})
+
+def about(request):
+    return render(request, 'mainapp/about.html', {'page': 'about'})
