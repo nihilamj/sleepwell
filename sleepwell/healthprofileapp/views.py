2cc5a160a775c6cd8fc6ba2b0e94d3ee5869929f
nihilamj <nihilamjmj@gmail.com>
2024-02-13 14:01:52 +0530

    13 FEB 2024
    Added the code for a successfull login and logout

--- sleepwell/healthprofileapp/views.py
+++ sleepwell/healthprofileapp/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
 from .forms import OccupationForm, HealthProfileSignUPForm,HealthProfileSignINForm
+from django.contrib import messages
 
+# Basic Functions
+
+def getUser(request):
+    if request.session.has_key('user_data'):
+        return request.session['user_data']
+
+
+def removeUser(request):
+    if request.session.has_key('user_data'):
+        del request.session['user_data']
 
 # Create your views here.
 
@@ -25,7 +36,10 @@ def signin(request):
         form = HealthProfileSignINForm(request.POST)
         print("1")
         if form.is_valid():
+            removeUser(request)
+            request.session['user_data'] = {'authenticated': True}
             return redirect('dashboard')  # Change 'dashboard' to the name of your dashboard URL
+            #return HttpResponseRedirect('/',request)
     else:
         form = HealthProfileSignINForm()
     return render(request, 'healthprofileapp/signin.html', {'page': 'signin','form': form})
@@ -33,13 +47,30 @@ def signin(request):
 
 
 def signout(request):
-    logout(request)  # Logout the user
+    if request.session.has_key('user_data'):
+        del request.session['user_data']
     return redirect('signin')  # Redirect to the sign-in page after logout
+    
 
 
 
 def dashboard(request):
-    return render(request, 'healthprofileapp/dashboard.html', {'page': 'dashboard'})
+    
+    data = {}
+
+    if 'user_data' in request.session:
+        if request.session['user_data'] is not None:
+            data['authenticated'] = request.session['user_data'].get('authenticated', False)
+        else:
+            # Handle the case where request.session['user_data'] is None
+            # For example, set data['authenticated'] to False or another default value
+            data['authenticated'] = False
+    else:
+        # Handle the case where 'user_data' doesn't exist in request.session
+        # For example, set data['authenticated'] to False or another default value
+        data['authenticated'] = False
+    
+    return render(request, 'healthprofileapp/dashboard.html', {'page': 'dashboard','data':data})
 
 
 
