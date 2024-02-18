3884c9d37c0d39213dccb7e72b122b3620067d61
nihilamj <nihilamjmj@gmail.com>
2024-02-18 18:56:39 +0530

    created healthrecord model

--- sleepwell/healthprofileapp/models.py
+++ sleepwell/healthprofileapp/models.py
@@ -33,3 +33,77 @@ class HealthProfile(models.Model):
         return super().save(*args, **kwargs)
 
 
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from .models import HealthProfile
+
+class HealthRecord(models.Model):
+
+    SLEEP_DISORDER_CHOICES = [
+        ('No', 'No'),
+        ('Insomnia', 'Insomnia'),
+        ('Sleep Apnea', 'Sleep Apnea'),
+        ('Narcolepsy', 'Narcolepsy'),
+        ('Restless Legs Syndrome (RLS)', 'Restless Legs Syndrome (RLS)'),
+        ('Periodic Limb Movement Disorder (PLMD)', 'Periodic Limb Movement Disorder (PLMD)'),
+        ('Parasomnias', 'Parasomnias'),
+        ('Circadian Rhythm Sleep Disorders', 'Circadian Rhythm Sleep Disorders'),
+        ('Hypersomnia', 'Hypersomnia'),
+        ('Sleep-related movement disorders', 'Sleep-related movement disorders'),
+        ('Sleep-related breathing disorders', 'Sleep-related breathing disorders'),
+    ]
+
+    BMI_LEVELS_CHOICES = [
+        ('Severe Thinness', 'Severe Thinness'),
+        ('Moderate Thinness', 'Moderate Thinness'),
+        ('Mild Thinness', 'Mild Thinness'),
+        ('Normal', 'Normal'),
+        ('Overweight', 'Overweight'),
+        ('Obese Class I', 'Obese Class I'),
+        ('Obese Class II', 'Obese Class II'),
+        ('Obese Class III', 'Obese Class III'),
+    ]
+
+    healthprofile = models.ForeignKey(HealthProfile, on_delete=models.CASCADE, related_name='sleep_records')
+    height = models.FloatField(verbose_name='Height', help_text='Enter height in meters', default=0,blank=False, null=False)
+    weight = models.FloatField(verbose_name='Weight', help_text='Enter weight in kilograms', default=0,blank=False, null=False)
+
+    physical_activity = models.PositiveIntegerField(verbose_name='Physical Activity', default=0, help_text='Enter level of physical activity in minutes',blank=False, null=False)
+    daily_steps = models.PositiveIntegerField(verbose_name='Daily Steps', help_text='Enter daily steps count', default=0,blank=False, null=False)
+
+    blood_pressure = models.CharField(verbose_name='Blood Pressure', max_length=20, help_text='Enter blood pressure measurement',default='120/80 mmHg',blank=False, null=False)
+    heart_rate = models.PositiveIntegerField(verbose_name='Heart Rate', help_text='Enter heart rate', default=0,blank=False, null=False)
+
+    sleep_duration = models.FloatField(verbose_name='Sleep Duration', help_text='Enter sleep duration in hours', default=0,blank=False, null=False)
+
+    quality_of_sleep = models.IntegerField(verbose_name='Quality of Sleep', help_text='Enter quality of sleep on a scale of 1 to 10', blank=True, null=True)
+    stress_level = models.IntegerField(verbose_name='Stress Level', help_text='Enter stress level on a scale of 1 to 10', blank=True, null=True)
+    bmi = models.CharField(verbose_name='BMI', max_length=50, choices=BMI_LEVELS_CHOICES, blank=True, null=True)
+    sleep_disorder = models.CharField(verbose_name='Sleep Disorder', max_length=50, choices=SLEEP_DISORDER_CHOICES , blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        # BMI Calculation and saving logic
+        if self.height > 0 and self.weight > 0:
+            bmi = self.weight / (self.height ** 2)
+            if bmi < 16:
+                self.bmi_category = 'Severe Thinness'
+            elif 16 <= bmi < 17:
+                self.bmi_category = 'Moderate Thinness'
+            elif 17 <= bmi < 18.5:
+                self.bmi_category = 'Mild Thinness'
+            elif 18.5 <= bmi < 25:
+                self.bmi_category = 'Normal'
+            elif 25 <= bmi < 30:
+                self.bmi_category = 'Overweight'
+            elif 30 <= bmi < 35:
+                self.bmi_category = 'Obese Class I'
+            elif 35 <= bmi < 40:
+                self.bmi_category = 'Obese Class II'
+            else:
+                self.bmi_category = 'Obese Class III'
+
+        super().save(*args, **kwargs)
+
+    
+
+
